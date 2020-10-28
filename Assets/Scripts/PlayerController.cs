dcea94fd4979ef60cc52d7b7672ab4781f7b3920
chris.homer <chris_homer93@hotmail.com>
2020-10-28 20:30:03 +0000

    Initial setup of map

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    //private Animator anim;
+    private NavMeshAgent agent;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        //anim = GetComponent<Animator>()	
+        agent = GetComponent<NavMeshAgent>();
+	}
+}
